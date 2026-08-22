5e94a613195fd7a5c4cd159695c2edb2c682b61b
Armish-W <f24bscs013@kinnaird.edu.pk>
2026-08-22 11:44:38 +0000

    Strings concatenation

---
+++ Python-Basics/strings.py
@@ -0,0 +1,19 @@
+text="icecream"
+print(text)
+print(text[1])
+print(text[0])
+text="hi A"
+print()
+print(text[:4])
+print(text[:3])
+print(text[0:])
+address= ''' 419/A JOhar Town
+RECHS
+Lahore'''
+print(address)
+s1="Hi"
+s2='A'
+print(s1+' '+s2)
+s2=24
+print(s2)
+print(s1+str(s2))
