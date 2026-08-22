54d7fc6ff1a77008a366c7f4eb30349e352ceeb7
Armish-W <f24bscs013@kinnaird.edu.pk>
2026-08-22 11:36:23 +0000

    Variables

---
+++ Python-Basics/variables.py
@@ -0,0 +1,23 @@
+rent=306.1
+gas=220.5
+groceries=306.1
+# from decimal import Decimal
+# rent = Decimal("306.1")
+rent= rent + rent + rent
+print(round(rent))
+total=rent + gas + groceries
+print(total)
+item1="rent"
+item2="gas"
+item3="groceries"
+print("Expense Items:",item1,item2,item3)
+rent, gas, groceries = 1220, 220.5, 306.1
+print(rent,gas,groceries)
+nyc_bal, bal_pit=188,247
+total_distance=nyc_bal+bal_pit
+print(total_distance)
+mph=65
+time=total_distance/mph
+print(time)
+print(round(time,2))
+print(round(6-5.7,1))
